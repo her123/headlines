5c9294fcc62beef0f17122015c130ab475452e24
Herrmann, Neville (PI, Black Mountain) <Neville.Herrmann@csiro.au>
2017-09-22 16:48:39 +1000

    Python3

--- headlines.py
+++ headlines.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
-import urllib2
 import urllib
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -15,16 +15,16 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 DEFAULTS = {'publication':'bbc',
             'city': 'Canberra,AU',
-	    'currency_from':'AUD',
+	        'currency_from':'AUD',
             'currency_to':'USD'}
 
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=7f5e16b6bbbd7b221d65067442a06485'
-CURRENCY_URL = 'https://openexchangerates.org//api/latest.json?app_id=f1045002e05a4fc792cd459c3785739c'
+CURRENCY_URL = 'https://openexchangerates.org/api/latest.json?app_id=f1045002e05a4fc792cd459c3785739c'
 
 def get_weather(query):
-    query = urllib.quote(query)
+    query = urllib.parse.quote(query)
     url = WEATHER_URL.format(query)
-    data = urllib2.urlopen(url).read()
+    data = urllib.request.urlopen(url).read()
     parsed = json.loads(data)
     weather = None
     if parsed.get("weather"):
@@ -34,42 +34,42 @@ def get_weather(query):
 
 @app.route("/")
 def home():
-	# get customised headlines, based on user input or default
-	publication = request.args.get('publication')
-	if not publication:
-		publication = DEFAULTS['publication']
-	articles = get_news(publication)
-	# get customised weather based on user input or default
-	city = request.args.get('city')
-	if not city:
-		city = DEFAULTS['city']		
-	weather = get_weather(city)
-        # get customized currency based on user input or default
-    	currency_from = request.args.get("currency_from")
-    	if not currency_from:
-        	currency_from = DEFAULTS['currency_from']
-    	currency_to = request.args.get("currency_to")
-    	if not currency_to:
-        	currency_to = DEFAULTS['currency_to']
-	rate, currencies = get_rate(currency_from, currency_to)
-    	return render_template("home.html", articles=articles,weather=weather, currency_from=currency_from, currency_to=currency_to,    				rate=rate,currencies=sorted(currencies))
+    # get customised headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customised weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']		
+    weather = get_weather(city)
+    # get customized currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+        currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles=articles,weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate,currencies=sorted(currencies))
 
 
 def get_news(query):
-  	if not query or query.lower() not in RSS_FEEDS:
-		publication = "bbc"
-	else: 
-		publication = query.lower()
-	feed = feedparser.parse(RSS_FEEDS[publication])
-	return feed["entries"]	
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else: 
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed["entries"]	
 
 def get_rate(frm, to):
-        all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    all_currency = urllib.request.urlopen(CURRENCY_URL).read()
 
-        parsed = json.loads(all_currency).get('rates')
-        frm_rate = parsed.get(frm.upper())
-        to_rate = parsed.get(to.upper())
-        return (to_rate / frm_rate, parsed.keys())
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate / frm_rate, parsed.keys())
 
 if __name__ == "__main__":
-  app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True)
